f8dc01ca0813133f420c3cffe7bc0ff2667559f9
AyoubKhaoua <mr.ayoubkhaoua@example.com>
2026-02-27 00:20:07 +0100

    feat: add custom requests for colocation, payment, expense and invitation validation

---
+++ app/Http/Requests/StoreColocationRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreColocationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'min:5', 'max:25'],
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Le nom est obligatoire.',
+            'name.min' => 'Le nom doit contenir au moins 5 caractères.',
+        ];
+    }
+}

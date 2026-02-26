6adbb5dd5e1c52ec77aeb9055c5f5264e3e1e40c
AyoubKhaoua <mr.ayoubkhaoua@example.com>
2026-02-27 00:14:20 +0100

    add show form method for colocation creation (store colocation & colocatioUser)

--- app/Http/Controllers/ColocationController.php
+++ app/Http/Controllers/ColocationController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreColocationRequest;
+use App\Models\Colocation;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Request;
 
 class ColocationController extends Controller
 {
     public function create()
     {
-        return redirect()->route('show.form');
+        return view('colocation.form');
     }
-    public function store(Request $req) {}
+    public function store(StoreColocationRequest $req)
+    {
+        $colocation = Colocation::create([
+            'name' => $req->name,
+            'status' => 'active',
+        ]);
+
+        $colocation->members()->attach(Auth::id(), [
+            'role' => 'owner',
+            'joined_at' => now(),
+        ]);
+        $id = Auth::id();
+        $user = User::find($id);
+        dd(auth()->user());
+
+        return redirect()->route('user.dashboard');
+    }
 }
